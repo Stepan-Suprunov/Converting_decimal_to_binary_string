f9e4f2309d0baab40283d251d59a17c99386a336
Stepan-Suprunov <ipsuprunovso@mail.ru>
2022-07-02 20:13:58 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Преобразование десятичного числа в двоичное через строку
+
+
+Console.WriteLine("Введите число");
+int number10 = int.Parse(Console.ReadLine());
+int number2;
+string result = string.Empty;
+
+for (; number10 > 0; number10 /= 2)
+{
+    number2 = number10%2;
+    result = number2 + result;
+}
+
+Console.WriteLine(result);
